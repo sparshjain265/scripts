adc9f3a2fb09b57284d12427ae024d28651ec9b0
Sparsh Jain <sparshjain.265@gmail.com>
2023-05-20 12:10:29 -0400

    restructure options.lua

--- nvim/lua/user/options.lua
+++ nvim/lua/user/options.lua
@@ -1,53 +1,61 @@
 -- :help options for generic help
 -- :help 'option' for help on that particular option
 
-vim.opt.compatible = false              -- disable compatibility to old time vi
+-- avoid vim.opt all the time
+-- define options locally first
+local options = {
+compatible = false,              -- disable compatibility to old time vi
 
-vim.opt.showmatch = true                -- show matching parenthesis
-vim.opt.ignorecase = true               -- case insensitive search
-vim.opt.smartcase = true                -- case sensitive if at least one capital letter
-vim.opt.mouse = 'a'                     -- enable mouse
-vim.opt.hlsearch = true                 -- highlight search
-vim.opt.incsearch = true                -- incremental search
+showmatch = true,                -- show matching parenthesis
+ignorecase = true,               -- case insensitive search
+smartcase = true,                -- case sensitive if at least one capital letter
+mouse = 'a',                     -- enable mouse
+hlsearch = true,                 -- highlight search
+incsearch = true,                -- incremental search
 
-vim.opt.smarttab = true                 -- tab also uses shiftwidth
-vim.opt.tabstop = 4                     -- tabsize
-vim.opt.softtabstop = 4                 -- soft tab stop
-vim.opt.expandtab = true                -- use spaces for tabs
-vim.opt.shiftwidth = 4                  -- width for indentation purposes
-vim.opt.autoindent = true               -- set new line in the same indentation as the previous line
-vim.opt.smartindent = true              -- smart indentation for programming
-vim.opt.number = true                   -- show line numbers
-vim.opt.relativenumber = true           -- show relative line numbers
+smarttab = true,                 -- tab also uses shiftwidth
+tabstop = 4,                     -- tabsize
+softtabstop = 4,                 -- soft tab stop
+expandtab = true,                -- use spaces for tabs
+shiftwidth = 4,                  -- width for indentation purposes
+autoindent = true,               -- set new line in the same indentation as the previous line
+smartindent = true,              -- smart indentation for programming
+number = true,                   -- show line numbers
+relativenumber = true,           -- show relative line numbers
+
+-- access to system clipboards
+clipboard = { "unnamed", "unnamedplus" },
+
+cursorline = true,               -- highlight cursor line
+cursorlineopt = 'number',        -- highlight only the line number of the cursorline
+ttyfast = true,                  -- speed up scrolling
+
+-- sane way of splitting panes
+splitright = true,
+splitbelow = true,
+
+-- set scrolloff to show the set number of lines/characters for context
+scrolloff = 8,
+sidescrolloff = 8,
+
+termguicolors = true,            -- brighter colors if terminal supports
+
+-- undofile = true                 -- persistent undo
+
+timeoutlen = 1000,               -- time to wait for a mapped sequence to complete (in milliseconds)
+updatetime = 300,                -- faster completion (4000ms by default)
+
+signcolumn = 'yes',              -- always show the sign column to avoid shifting the text every now and then
+}
+
+for key, value in pairs(options) do
+    vim.opt[key] = value
+end
 
 -- detect filetype, detect corresponding plugins and intendation
 vim.cmd "filetype plugin indent on"
 -- enable syntax highlighting
 vim.cmd "syntax on"
-
--- access to system clipboards
-vim.opt.clipboard = vim.opt.clipboard + { "unnamed", "unnamedplus" }
-
-vim.opt.cursorline = true               -- highlight cursor line
-vim.opt.cursorlineopt = 'number'        -- highlight only the line number of the cursorline
-vim.opt.ttyfast = true                  -- speed up scrolling
-
--- sane way of splitting panes
-vim.opt.splitright = true
-vim.opt.splitbelow = true
-
--- set scrolloff to show the set number of lines/characters for context
-vim.opt.scrolloff = 8
-vim.opt.sidescrolloff = 8
-
-vim.opt.termguicolors = true            -- brighter colors if terminal supports
-
--- vim.opt.undofile = true                 -- persistent undo
-
-vim.opt.timeoutlen = 1000               -- time to wait for a mapped sequence to complete (in milliseconds)
-vim.opt.updatetime = 300                -- faster completion (4000ms by default)
-
-vim.opt.signcolumn = 'yes'              -- always show the sign column to avoid shifting the text every now and then
 
 -- allow arrow keys to wrap over first/last characters of the line to move to previous/next lines
 vim.cmd "set whichwrap+=<,>,[,]"
