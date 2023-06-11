a03c3bff07a9bfc640bff4a9e481b175d52009f1
Sparsh Jain <sparshjain.265@gmail.com>
2023-06-11 00:10:37 -0400

    add :MasonUpdate as run on installation

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -93,7 +93,10 @@ return packer.startup(function(use)
 
 	-- LSP
 	use("neovim/nvim-lspconfig")
-	use("williamboman/mason.nvim") -- simple to use language server installer
+	use({
+		"williamboman/mason.nvim",
+		run = ":MasonUpdate",
+	}) -- simple to use language server installer
 	use("williamboman/mason-lspconfig.nvim")
 	use("jose-elias-alvarez/null-ls.nvim") -- LSP diagnostics and code actions
 
