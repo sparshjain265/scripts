909606c162fea9268250027ffc1d11ae1ff18c0a
Sparsh Jain <sparshjain.265@gmail.com>
2023-06-15 09:00:34 -0400

    minor update in the start screen

--- nvim/lua/user/alpha.lua
+++ nvim/lua/user/alpha.lua
@@ -32,7 +32,8 @@ local function footer()
 	-- local fortune = handle:read("*a")
 	-- handle:close()
 	-- return fortune
-	return "chrisatmachine.com"
+	-- return "chrisatmachine.com"
+	return ""
 end
 
 dashboard.section.footer.val = footer()
