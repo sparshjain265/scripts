a9b85912e162286e59425bd24d7fbde734604c4c
Sparsh Jain <sparshjain.265@gmail.com>
2023-10-03 19:46:47 -0700

    update neovim plugins

--- nvim/lua/user/lsp/init.lua
+++ nvim/lua/user/lsp/init.lua
@@ -1,8 +1,9 @@
-local statusOk, _ = pcall(require, 'lspconfig')
+local statusOk, _ = pcall(require, "lspconfig")
 if not statusOk then
-    return
+	return
 end
 
-require('user.lsp.mason')
-require('user.lsp.handlers').setup()
-require 'user.lsp.null-ls'
+require("user.lsp.mason")
+require("user.lsp.handlers").setup()
+require("user.lsp.null-ls")
+require("user.lsp.jdtls")
--- nvim/lua/user/lsp/mason.lua
+++ nvim/lua/user/lsp/mason.lua
@@ -1,49 +1,52 @@
 local servers = {
-    'pyright',
-    'tsserver',
-    'jsonls',
-    'html',
-    'lua_ls',
+	"clangd",
+	-- "jdtls",
+	"pyright",
+	"tsserver",
+	"jsonls",
+	"html",
+	"lua_ls",
+	"texlab",
 }
 
 local settings = {
-    ui = {
-        border = 'none',
-        icons = {
-            package_installed = "◍",
+	ui = {
+		border = "none",
+		icons = {
+			package_installed = "◍",
 			package_pending = "◍",
 			package_uninstalled = "◍",
-        },
-    },
-    log_level = vim.log.levels.INFO,
-    max_concurrent_installers = 4,
+		},
+	},
+	log_level = vim.log.levels.INFO,
+	max_concurrent_installers = 4,
 }
 
-require('mason').setup(settings)
-require('mason-lspconfig').setup({
-    ensure_installed = servers,
-    automatic_installation = true,
+require("mason").setup(settings)
+require("mason-lspconfig").setup({
+	ensure_installed = servers,
+	automatic_installation = true,
 })
 
-local lspconfigStatusOk, lspconfig = pcall(require, 'lspconfig')
+local lspconfigStatusOk, lspconfig = pcall(require, "lspconfig")
 if not lspconfigStatusOk then
-    return
+	return
 end
 
 local opts = {}
 
 for _, server in pairs(servers) do
-    opts = {
-        on_attach = require('user.lsp.handlers').on_attach,
-        capabilities = require('user.lsp.handlers').capabilities,
-    }
+	opts = {
+		on_attach = require("user.lsp.handlers").on_attach,
+		capabilities = require("user.lsp.handlers").capabilities,
+	}
 
-    server = vim.split(server, '@')[1]
+	server = vim.split(server, "@")[1]
 
-    local requireOk, confOpts = pcall(require, 'user.lsp.settings.' .. server)
-    if requireOk then
-        opts = vim.tbl_deep_extend('force', confOpts, opts)
-    end
+	local requireOk, confOpts = pcall(require, "user.lsp.settings." .. server)
+	if requireOk then
+		opts = vim.tbl_deep_extend("force", confOpts, opts)
+	end
 
-    lspconfig[server].setup(opts)
+	lspconfig[server].setup(opts)
 end
